0cde716ead7383e2a3c2fb7ef0c0d0797f6c2c48
Veemon <xxtheforgottenliesxx@gmail.com>
2017-12-02 01:06:11 +1000

    yea

---
+++ evolve.py
@@ -0,0 +1,102 @@
+'''
+-crossover- TODO
+
+structure -mutations- occur in two ways:
+
+    in -add connection- mutation, a single new connection gene with a random weight is added
+    connecting two previously unconnected nodes
+
+    in the -add node- mutation, an existing connection is split and the new node placed where
+    the old connection used to be.
+        the old connection is disabled
+        the new connections are then added
+            - the new connection leading into the new node has a weight of 1
+            - the new connection leading out of the new node has the weight of the old connection
+
+whenever a new gene appears throught structural mutation, a global innovation number is incremented and assigned to that gene
+
+it is possible to ensure that when the same structure arises more than once through independent mutations
+in the same generation, each identical mutation is assigned the same innovation number.
+
+when -crossover- occurs, the genes in both genomes with the same innovation numbers are lined up.
+these are called matching genes.
+
+genes that do not match are either disjoint or excess, depending on whether they occur within or outside the range of the parents
+innovation numbers.
+
+when composing offspring, genes are randomly chosen from either parent at matching genes,
+whereas all excess or disjoint genes are always included from the more fit parent.
+
+the number of excess and disjoint genes between a pair of genomes is a natural measure of their compatibility distance.
+the more disjoint two genomes are, the less evolutionary history they share, and thus the less compatibile they are.
+
+sigma = c1(E/N) + c2(D/N) + c3*W
+sigma is the compatibility difference
+c1,c2,c3 are coefficients
+E is the number of excess genes
+D is the number of disjoint genes
+W is the average weight differences of matching genes (including disable genes)
+N is the number of genes in the larger genome
+
+this compatibility difference allows for speciation, with a compatibility threshold sigma_t.
+
+in each generation genomes are sequentially placed into species.
+each existing species is represented by a random genome inside the species from the previous generation.
+
+a given genome g in the current generation is placed in the first species with which g is compatibile with
+the respective genome of that species.
+if g is not compatibile with any of the existing species, a new species is created with g as the representative.
+
+explicit fitness sharing is used, where organisms in the same species must share the fitness of their niche.
+this is to combat dominant species.
+
+the adjusted fitness for the organism is calculated according to its compatibility distance from every other organism
+in the population.
+
+f_i' = f_i / sum( sh( sigma(i,j) ) )
+f_i' is the adjusted fitness for organism i
+f_i = is the fitness for organism i
+sigma(i,j) is the compatibility distance between organisms i and j
+
+sh is the sharing function
+    it is set to 0 when the compatibility distance is above the threshold
+    otherwise it is set to 1
+
+species then reproduce by first eliminating the lowest performing members from the population.
+the entire population is then replaced by the offspring of the remaining organisms in each species.
+
+neat starts out with a uniform population of networks with zero hidden nodes.
+all inputs are directly connected to all outputs.
+'''
+
+'''
+Creates a population of (randomly generated) members
+Scores each member of the population based on some goal. This score is called a fitness function.
+Selects and breeds the best members of the population to produce more like them
+Mutates some members randomly to attempt to find even better candidates
+Kills off the rest (survival of the fittest and all), and
+Repeats from step 2. Each iteration through these steps is called a generation.
+'''
+
+# Hyper-parameters
+input_nodes = 4
+output_nodes = 2
+
+population_size = 1000
+num_generations = 100
+
+# Create Population
+population = []
+for i in range(population_size):
+    genome = 0
+    population.append(genome)
+
+for generation in range(num_generations):
+
+    # Measure Fitness
+    for genome in population:
+        genome.evaluate()
+
+    # Eliminate Worst
+
+    # Create offspring
--- genome.py
+++ genome.py
@@ -1,3 +1,345 @@
-# make a genome class so the network can build from it
-# implement the differenct types of crossover and mutation
-# also read the paper and take notes????
+# standard library
+from collections import namedtuple
+from enum import Enum
+from math import exp
+import sys
+
+# third-party library
+from tabulate import tabulate
+
+
+
+
+# Helper Abstractions
+Neuron = namedtuple('Neuron',
+                        ['type', 'id'])
+
+Connection = namedtuple('Collection',
+                        ['input', 'output', 'weight', 'enabled', 'innovation'])
+
+class Type(Enum):
+    IN = 0
+    HIDDEN = 1
+    OUT = 2
+
+
+
+
+# Activation Functions
+def sigmoid(x):
+    return 2 / (1 + exp(-4.9*x)) - 1
+
+def relu(x):
+    return x if x > 0 else 0
+
+# Nodes - acts like an I/O processor
+class Node():
+    def __init__(self, idx, node_type):
+        self.id = idx
+        self.type = node_type
+
+        self.cache = []
+        self.output = 0
+        self.ready = False
+
+        self.input_counter = 0
+        self.input_nodes = []
+
+        self.weights = []
+        self.output_nodes = []
+
+        self.recurrent_out = []
+
+    def detect_recurrence(self):
+        # recurrence with output nodes
+        if self.type == Type.OUT:
+
+            # we assume that all output nodes must be recurrent
+            if len(self.output_nodes) > 0:
+                for node in self.output_nodes:
+                    node.input_nodes.remove(self)
+                    self.recurrent_out.append(node)
+                self.output_nodes = []
+
+        # recurrence with hidden nodes
+        elif self.type == Type.HIDDEN:
+
+            # check if node is in inputs
+            for node1 in self.output_nodes:
+                if node1 in self.input_nodes:
+
+                    # check if that node, has an output to this node
+                    for node2 in node1.output_nodes:
+                        if node2.id == self.id:
+                            # remove input node in-connection
+                            self.input_nodes.remove(node1)
+
+                            # shuffle output node to recurrent
+                            node1.output_nodes.remove(self)
+                            node1.recurrent_out.append(self)
+
+        # recurrence with input nodes
+        elif self.type == Type.IN:
+
+            # we assume that all input nodes must be recurrent
+            if len(self.input_nodes) > 0:
+                for node in self.input_nodes:
+                    # shuffle output node to recurrent
+                    node.output_nodes.remove(self)
+                    node.recurrent_out.append(self)
+
+                    # remove node connection
+                    self.input_nodes.remove(node)
+
+    def forward(self):
+        # if we've received all inputs
+        if len(self.cache) >= self.input_counter and self.ready == False:
+
+            # forward our output to all nodes
+            output = sigmoid(sum(self.cache))
+            for i, node in enumerate(self.output_nodes):
+                node.cache.append(output * self.weights[i])
+                node.input_counter += 1
+
+            # store the output for recurrent pass
+            self.output = output
+            self.cache = []
+
+            # signal that this node has no jobs
+            self.ready = True
+            self.input_counter = 0
+
+    def r_forward(self):
+        # forward the last output to recurrent connections
+        l = len(self.output_nodes)
+        for i, node in enumerate(self.recurrent_out):
+            node.cache.append(self.output * self.weights[l + i])
+
+    def reset(self):
+        self.cache = []
+        self.output = 0
+        self.input_counter = 0
+        self.ready = False
+
+# Network - builds the routing between nodes
+class Network():
+    def __init__(self, genome):
+        # neurons
+        self.nodes = []
+        for neuron in genome.neurons:
+            self.nodes.append(Node(neuron.id, neuron.type))
+
+        # connections
+        self.connections = genome.connections
+
+    def find_node(self, node_id):
+        # TODO: sort when copying from genome,
+        #       then index into the array/dict instead.
+        for node in self.nodes:
+            if node.id == node_id:
+                return node
+
+    def build(self):
+        # build all neuron connections
+        for connection in self.connections:
+            if connection.enabled == True:
+                # find corresponding node
+                input_node = self.find_node(connection.input)
+                output_node = self.find_node(connection.output)
+
+                # update connection
+                input_node.weights.append(connection.weight)
+                input_node.output_nodes.append(output_node)
+                output_node.input_nodes.append(input_node)
+
+        # after all connections are done, detect cycles
+        for node in self.nodes:
+            node.detect_recurrence()
+
+    def forward(self, x):
+        # get input and output nodes
+        input_nodes = []
+        output_nodes = []
+        for node in self.nodes:
+            if node.type == Type.In:
+                input_nodes.append(node)
+            elif node.type == Type.OUT:
+                output_nodes.append(node)
+
+        # split x to feed to input neurons
+        for i, node in enumerate(input_nodes):
+            node.cache.append(x[i])
+
+        # begin forward pass 1 - propagation
+        length = len(output_nodes)
+        while True:
+
+            # forward propagate some nodes
+            for node in self.nodes:
+                node.forward()
+
+            # check if the output nodes are ready
+            num_ready = 0
+            for node in output_nodes:
+                if node.ready == True:
+                    num_ready += 1
+            if num_ready == length:
+                break
+
+        # reset node state signals
+        for node in self.nodes:
+            node.ready = False
+
+        # begin forward pass 2 - recurrence
+        for node in self.nodes:
+            node.r_forward()
+
+        # return output
+        return [x.output for x in output_nodes]
+
+    def reset(self):
+        for node in self.nodes:
+            node.reset()
+
+    def __str__(self):
+        # shortcut
+        nodes = self.nodes
+
+        # tabulate headers
+        headers = ['ID', 'IN', 'OUT', 'R']
+
+        # all node id's
+        node_ids = [x.id for x in nodes]
+
+        # all respective node inputs
+        node_inputs = []
+        for node in nodes:
+            inputs = []
+            for node_in in node.input_nodes:
+                inputs.append(node_in.id)
+            node_inputs.append(inputs)
+
+        # all respective node outputs
+        node_outputs = []
+        for node in nodes:
+            outputs = []
+            for node_out in node.output_nodes:
+                outputs.append(node_out.id)
+            node_outputs.append(outputs)
+
+        # all respective node recurrents
+        recurrents = []
+        for node in nodes:
+            recur = []
+            for node_rec in node.recurrent_out:
+                recur.append(node_rec.id)
+            recurrents.append(recur)
+
+        # swizzle into mega list
+        swizzle = []
+        for i in range(len(nodes)):
+            current_node_info = []
+            current_node_info.append([node_ids[i]])
+            current_node_info.append(node_inputs[i])
+            current_node_info.append(node_outputs[i])
+            current_node_info.append(recurrents[i])
+            swizzle.append(current_node_info)
+
+        return tabulate(swizzle, headers=headers, tablefmt="fancy_grid")
+
+
+
+
+class Genome:
+    def __init__(self):
+        # information
+        self.neurons = []
+        self.connections = []
+
+        # measurements
+        self.network = 0
+        self.fitness = 0
+
+    def build(self):
+        self.network = Network(self)
+        self.network.build()
+
+    def evaluate(self):
+        pass
+
+    def __str__(self):
+        headers = ['Nodes', 'Connections']
+        connection_str = [str([x.input, x.output])[1:-1] for x in self.connections]
+        for i, connection in enumerate(connection_str):
+            connection_str[i] = connection.replace(',', ' ->')
+
+        swizzle = []
+        l_node = len(self.neurons)
+        l_con = len(connection_str)
+        length = l_node if l_node > l_con else l_con
+
+        for i in range(length):
+            a = ''
+            b = ''
+
+            if i < l_node:
+                a = str(self.neurons[i].id)
+
+            if i < l_con:
+                b = connection_str[i]
+
+            swizzle.append([a,b])
+
+        return tabulate(swizzle, headers=headers)
+
+def crossover(g1, g2):
+    child = Genome()
+
+    #
+
+def mutate(g):
+    pass
+
+# setup parent 1
+g1 = Genome()
+
+g1.neurons.append(Neuron(Type.IN,1))
+g1.neurons.append(Neuron(Type.IN,2))
+g1.neurons.append(Neuron(Type.IN,3))
+g1.neurons.append(Neuron(Type.OUT,4))
+g1.neurons.append(Neuron(Type.HIDDEN,5))
+
+g1.connections.append(Connection(1,4,0.5,True,1))
+g1.connections.append(Connection(2,4,0.5,False,2))
+g1.connections.append(Connection(3,4,0.5,True,3))
+g1.connections.append(Connection(2,5,0.5,True,4))
+g1.connections.append(Connection(5,4,0.5,True,5))
+g1.connections.append(Connection(1,5,0.5,True,8))
+
+print(g1,'\n')
+
+# setup parent 2
+g2 = Genome()
+
+g2.neurons.append(Neuron(Type.IN,1))
+g2.neurons.append(Neuron(Type.IN,2))
+g2.neurons.append(Neuron(Type.IN,3))
+g2.neurons.append(Neuron(Type.OUT,4))
+g2.neurons.append(Neuron(Type.HIDDEN,5))
+g2.neurons.append(Neuron(Type.HIDDEN,6))
+
+g2.connections.append(Connection(1,4,0.5,True,1))
+g2.connections.append(Connection(2,4,0.5,False,2))
+g2.connections.append(Connection(3,4,0.5,True,3))
+g2.connections.append(Connection(2,5,0.5,True,4))
+g2.connections.append(Connection(5,4,0.5,False,5))
+g2.connections.append(Connection(5,6,0.5,True,6))
+g2.connections.append(Connection(6,4,0.5,True,7))
+g2.connections.append(Connection(3,5,0.5,True,9))
+g2.connections.append(Connection(1,6,0.5,True,10))
+
+print(g2,'\n')
+
+# crossover
+g3 = crossover(g1,g2)
+print(g3,'\n')
